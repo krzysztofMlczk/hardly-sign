f3734ca00b4b2f0eb522cf8e91a375a78668da3b
Marcin <marcinsz9045@gmail.com>
2022-05-15 13:09:53 +0200

    nullable fields

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -9,8 +9,8 @@ from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
 
     email = models.EmailField(_("email address"), unique=True, blank=False, null=False)
     is_staff = models.BooleanField(default=False)
